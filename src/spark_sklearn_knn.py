5ee3f7986e6de3039adee4613079cf343fd3df6a
Fang-Yi Chiu <11733504+fychiu@users.noreply.github.com>
2018-03-11 00:12:05 -0800

    update to delete redundant function

--- src/spark_sklearn_knn.py
+++ src/spark_sklearn_knn.py
@@ -1,11 +1,12 @@
 import numpy as np
+import time
 from pyspark import SparkContext, SparkConf
 from sklearn.neighbors import NearestNeighbors
 
 
-n_query = 200
+n_query = 2700
 n_feature = 100
-train_data = np.random.uniform(size=(10000, n_feature))
+train_data = np.random.uniform(size=(2000000, n_feature))
 query_data = np.array(train_data[-n_query:])
 assert (train_data[-n_query:] == query_data).all()
 k = 2
@@ -17,10 +18,6 @@ class pseudo_KNN():
         self.k = k
         return
 
-    def euclidean_distance_sqr(self, x, y):
-        return np.sum((x - y)**2, axis=1)
-    
-    
     def find(self, x):
         assert type(x) == type(np.zeros((1,))), x
         x = np.reshape(x, (-1, n_feature)) 
@@ -42,4 +39,9 @@ class pseudo_KNN():
         return indices, distances
 
 KNN = pseudo_KNN(k, train_data)
+print('Start query')
+start = time.time()
 indices, distances = KNN.query(query_data)
+print(sorted(indices, key=lambda x: x), len(indices))
+print(sorted(distances, key=lambda x: x), len(distances))
+print('Spend %.4f' % (time.time() - start))
